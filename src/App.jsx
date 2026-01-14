420ce56e2f6eaeabfdf18285a9eaccf319c3d4f9
sandun440 <sameerasandun440@gmail.com>
2026-01-14 16:06:11 +0530

    add footer and nav bar✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 
 function App() {
 
   return (
     <>
-      <p>hello</p>
+    <BrowserRouter>
+      <Navbar/>
+      <Footer/>
+    </BrowserRouter>
     </>
   )
 }
